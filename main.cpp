0b5719d0ec7d7cc30005065bdfd7b54b61b316f3
RC-corp-prog <vova.pavlikov.2000@mail.ru>
2024-07-01 19:16:25 +0300

    Delete function summ

--- main.cpp
+++ main.cpp
@@ -2,16 +2,10 @@
 using namespace std;
 
 void print();
-void summ(int a, int b);
 
 
 int main()
 {
     cout<<"Hello"<<endl;
     return 0;
-}
-
-void summ(int a, int b)
-{
-    cout<<a+b<<endl;
 }
