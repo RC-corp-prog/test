58ab892bf5b8651cd772e2da6c10f1d21629ac3e
RC-corp-prog <vova.pavlikov.2000@mail.ru>
2024-07-01 19:00:02 +0300

    change function print

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void print();
+void summ(int a, int b);
+
+
+int main()
+{
+    cout<<"Hello"<<endl;
+    return 0;
+}
+
+void summ(int a, int b)
+{
+    cout<<a+b<<endl;
+}
